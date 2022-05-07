5ea5c19c90233fbff0a89fed317de88e7f49582a
Naveen2890 <104992020+Naveen2890@users.noreply.github.com>
2022-05-07 16:23:22 +0530

    Add files via upload

---
+++ file extension.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 11:48:24 2022
+
+@author: shashank
+"""
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : ", f_extns[-1])
