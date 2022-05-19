1810b4e43cc710c0c2a5c084f31ef61b1b3254be
Naveen2890 <104992020+Naveen2890@users.noreply.github.com>
2022-05-19 19:00:51 +0530

    Add files via upload

---
+++ positivenumb.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+list=[1,2,-3,4,5]
+print(list)
+for i in list:
+    if i<=0:
+        list.remove(i)
+        
+print(list)
